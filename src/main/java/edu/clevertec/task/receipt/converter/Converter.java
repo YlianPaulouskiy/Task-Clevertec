9ad9cf5882a096cdb84bcf27bde97e714614520d
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-12-15 14:21:14 +0300

    add method getProducts

--- src/main/java/edu/clevertec/task/receipt/converter/Converter.java
+++ src/main/java/edu/clevertec/task/receipt/converter/Converter.java
@@ -1,15 +1,24 @@
 package edu.clevertec.task.receipt.converter;
 
+import edu.clevertec.task.model.Product;
 import edu.clevertec.task.receipt.exception.IncorrectInputSourceException;
+import edu.clevertec.task.receipt.exception.ProductNotFoundException;
 import edu.clevertec.task.receipt.lines.LineCheck;
 import edu.clevertec.task.receipt.lines.LineParsing;
+import edu.clevertec.task.repository.ProductRepository;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Converter {
 
-    public Map<Long, Integer> getProducts(String source) {
+    private final ProductRepository productRepository;
+
+    public Converter(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Map<Long, Integer> getProductIds(String source) {
         if (LineCheck.isCorrectSource(source)) {
             Map<Long, Integer> products = new HashMap<>();
             //разделяем по пробелу
@@ -38,6 +47,27 @@ public class Converter {
                 }
             }
             return null;
+        } else {
+            throw new IncorrectInputSourceException("Incorrect source\n" + source
+                    + "\nsource will be match regular expression:\n(\\d-\\d\\s*)+(card-\\d{4})?");
+        }
+    }
+
+    public Map<Product, Integer> getProducts(String source) {
+        if (LineCheck.isCorrectSource(source)) {
+            Map<Long, Integer> productsIds = getProductIds(source);
+            Map<Product, Integer> products = new HashMap<>();
+            for (Long productId : productsIds.keySet()) {
+                if (productRepository.existsById(productId)) {
+                    products.put(
+                            productRepository.getReferenceById(productId),
+                            productsIds.get(productId)
+                    );
+                } else {
+                    throw new ProductNotFoundException("Product " + productId + " not founded.");
+                }
+            }
+            return products;
         } else {
             throw new IncorrectInputSourceException("Incorrect source\n" + source
                     + "\nsource will be match regular expression:\n(\\d-\\d\\s*)+(card-\\d{4})?");
