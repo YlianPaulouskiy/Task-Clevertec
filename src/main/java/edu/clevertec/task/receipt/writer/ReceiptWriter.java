920acdc90ff96d3932b05ffc429f4bed50e90948
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-12-19 20:16:30 +0300

    Add ReceiptWriterTest

--- src/main/java/edu/clevertec/task/receipt/writer/ReceiptWriter.java
+++ src/main/java/edu/clevertec/task/receipt/writer/ReceiptWriter.java
@@ -1,13 +1,20 @@
 package edu.clevertec.task.receipt.writer;
 
+import org.springframework.stereotype.Component;
+
 import java.io.FileWriter;
 import java.io.IOException;
-
 
 /**
  * Класс для записи в файл
  */
+@Component
 public class ReceiptWriter {
+
+    /**
+     * Путь, куда сохраняется чек
+     */
+    public static final String SAVE_PATH = "D:\\java\\my projects\\taskClevertec\\build\\resources\\main\\receipt.txt";
 
     /**
      * Сохраняет переданную строку в файл
@@ -15,9 +22,7 @@ public class ReceiptWriter {
      * @param source Строка, которую нужно сохранить
      */
     public void save(String source) {
-        try (FileWriter fileWriter = new FileWriter(
-                // путь куда сохраняется чек
-                "D:\\java\\my projects\\taskClevertec\\build\\resources\\main\\receipt.txt")) {
+        try (FileWriter fileWriter = new FileWriter(SAVE_PATH)) {
             fileWriter.write(source);
         } catch (IOException exception) {
             exception.printStackTrace();
---
+++ src/test/java/edu/clevertec/task/receipt/writer/ReceiptWriterTest.java
@@ -0,0 +1,51 @@
+package edu.clevertec.task.receipt.writer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.setMaxLengthForSingleLineDescription;
+
+@SpringBootTest
+class ReceiptWriterTest {
+
+    @Autowired
+    private ReceiptWriter receiptWriter;
+
+    @BeforeEach
+    void setUp() {
+        try (FileWriter writer = new FileWriter(ReceiptWriter.SAVE_PATH)) {
+            writer.write("");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        try (FileWriter writer = new FileWriter(ReceiptWriter.SAVE_PATH)) {
+            writer.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void save() {
+        try(BufferedReader reader = new BufferedReader(new FileReader(ReceiptWriter.SAVE_PATH))) {
+            String source = "SOMETHING TEXT";
+            //пуст ли файл
+            assertThat(reader.read()).isEqualTo(-1);
+            receiptWriter.save(source);
+            //не пуст после записи
+            assertThat(reader.readLine()).isEqualTo(source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
