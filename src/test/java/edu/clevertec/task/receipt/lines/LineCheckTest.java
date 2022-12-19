faaa0c5a6a9716d46a87d704d21ce837edcc1749
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-12-19 19:23:41 +0300

    add LineCheckTest

---
+++ src/test/java/edu/clevertec/task/receipt/lines/LineCheckTest.java
@@ -0,0 +1,41 @@
+package edu.clevertec.task.receipt.lines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LineCheckTest {
+
+    @Test
+    void isCorrectSource() {
+        assertThat(LineCheck.isCorrectSource("4-1 3-2 card-1111")).isEqualTo(true);
+        assertThat(LineCheck.isCorrectSource("10-10 3-5 card-1234")).isEqualTo(true);
+        assertThat(LineCheck.isCorrectSource("-1")).isEqualTo(false);
+        assertThat(LineCheck.isCorrectSource("card-1234")).isEqualTo(false);
+        assertThat(LineCheck.isCorrectSource("")).isEqualTo(false);
+        assertThat(LineCheck.isCorrectSource("card-1234 4-1 5-2")).isEqualTo(false);
+        assertThat(LineCheck.isCorrectSource("4-2 card-12345")).isEqualTo(false);
+    }
+
+    @Test
+    void isCardSource() {
+        assertThat(LineCheck.isCardSource("card-1234")).isEqualTo(true);
+        assertThat(LineCheck.isCardSource("")).isEqualTo(false);
+        assertThat(LineCheck.isCardSource("1234")).isEqualTo(false);
+    }
+
+    @Test
+    void isCardReceipt() {
+        assertThat(LineCheck.isCardReceipt(new String[] {"4-2", "2-5", "card-1234"})).isEqualTo(true);
+        assertThat(LineCheck.isCardReceipt(new String[] {"4-2", "2-5", "car1234"})).isEqualTo(false);
+        assertThat(LineCheck.isCardReceipt(new String[] {"4-2", "2-5", ""})).isEqualTo(false);
+        assertThat(LineCheck.isCardReceipt(new String[] {})).isEqualTo(false);
+    }
+}
